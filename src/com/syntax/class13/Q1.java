0e8017c1e65a37b6017db076f4c964f7f1d302a2
satasoy6 <atasoyseyma@yahoo.com>
2020-03-20 23:23:51 -0400

    IQ1

--- src/com/syntax/class13/Q1.java
+++ src/com/syntax/class13/Q1.java
@@ -15,17 +15,46 @@ public class Q1 {
 		int a = 10;
 		int b = 20;
 		
-		a = a+b; //30
-		b = a-b;//30-20=10
+		a = a+b;//30
+		b = a-b;//30-10=20
 		a = a-b;
+		
+		
 		
 		System.out.println("The value of a ="+a);
 		System.out.println("The value of b ="+b);
 		
-		String str1 = "Day";
-		String str2 = "Night";
+		//Swaping the value of strings
 		
-		str1  str
+		String str1 = "Syntax";
+		String str2 = "Technologies";
+		
+		str1 = str1+str2; // DayNight
+		
+		System.out.println(str1);
+		
+		str2 = str1.substring(0, str1.length()-str2.length());
+		System.out.println(str2);
+		
+		str1 = str1.substring(str2.length());// If we write it this way it will work with every word
+				System.out.println(str1);
+
+				
+				// Another way
+				
+				String s1 = "Hello";
+				String s2 = "Bye";
+				
+				String shorterstr1 = s1 + s2 ;
+				
+				s2 = shorterstr1.replace(s2,"");
+				s1 = shorterstr1.replace(s1, "");
+				
+		
+		
+		
+		
+		
 	}
 
 }
