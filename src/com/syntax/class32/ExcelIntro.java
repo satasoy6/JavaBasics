1857836100541058c5badbd1838b26a85fa42fb2
satasoy6 <atasoyseyma@yahoo.com>
2020-04-27 00:18:42 -0400

    Reading Excel

--- src/com/syntax/class32/ExcelIntro.java
+++ src/com/syntax/class32/ExcelIntro.java
@@ -14,21 +14,48 @@ public class ExcelIntro {
 		
 		String filePath = "/Users/seymaatasoy/Downloads/Test.xlsx";
 		
-		FileInputStream obj = new FileInputStream(filePath);
+		FileInputStream fis = new FileInputStream(filePath);
+		
 		//Accessing workbook
-		Workbook wbook = new XSSFWorkbook(obj);
+		
+		Workbook wbook = new XSSFWorkbook(fis);
+		
 		//Accessing sheet
 		Sheet sheet =wbook.getSheet("Sheet1");//always import apache poi sss model
+		
 		//Accessing row
-		Row row1 = sheet.getRow(0);
+		
+		Row row1 =sheet.getRow(0);
+		//Once you have access to the row you can have access to the cell
 		//Accessing cell
 		Cell cell = row1.getCell(1);
+		
 		//Get value from cell
 		String r1cell2 = cell.toString();
-		System.out.println(r1cell2);//LastName
+		System.out.println(r1cell2);
 		
-		//hoe to get New york
+		//how to get Newyork
 		
+		Row row3 = sheet.getRow(2);
+		Cell cell3=row3.getCell(2);
+		String r3c3 = cell3.toString();
+		System.out.println(r3c3);
+		
+		//how to get Virginia
+		// To get it with method chaining
+		
+		System.out.println(sheet.getRow(1).getCell(3).toString());
+		
+		
+		//To retrieve values based on type
+		//getNumericCellValue will print as it is
+		double cellv = sheet.getRow(1).getCell(4).getNumericCellValue();
+		System.out.println((int)cellv);
+		
+		//how to get 20151 in a string format
+		String valC = sheet.getRow(1).getCell(4).toString();
+		String [] arr = valC.split("\\.");
+		System.out.println(arr[0]);
 	}
 
 }
--- src/com/syntax/class32/ReadExcel.java
+++ src/com/syntax/class32/ReadExcel.java
@@ -10,13 +10,36 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ReadExcel {
 	public static void main(String[] args) throws IOException {
 		
-		String filePath="";
-		FileInputStream fis = new FileInputStream(filePath);
+		String filePath = "/Users/seymaatasoy/Documents/Test.xlsx";
+		FileInputStream fis = new FileInputStream (filePath);
 		
+		//to get an object of Workbook type
 		Workbook book = new XSSFWorkbook(fis);
-		Sheet sheet=book.getSheet("Sheet2");
-	}
+		Sheet sheet = book.getSheet("Sheet1");//to get access to the sheet
+		//When you import Sheet select apache poi ss that is matching with Workbook
+		
+		//need to find numbers or rows
+		int rows = sheet.getPhysicalNumberOfRows();
+		
+		//find number of columns
+		int colls = sheet.getRow(0).getLastCellNum();
+		
+		for(int r = 0;r<rows ; r++) {//iterates over rows
+			for(int c=0; c<colls ; c++) {
+				String cellVal = sheet.getRow(r).getCell(c).toString();
+				System.out.print(cellVal+" ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+	
 	
 	
 
+}
 }
